e824683847a3e559cfd4d2ad1ae59710cd460b85
Samiran Deb <samiran.deb@ericsson.com>
2022-01-11 17:20:49 +0530

    API key fix 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  env: {
+  publicRuntimeConfig: {
     NUXT_ENV_API_KEY: process.env.NUXT_ENV_API_KEY
-
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
